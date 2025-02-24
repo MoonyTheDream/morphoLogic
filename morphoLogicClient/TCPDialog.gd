f14e96ad80f0b61fc4350400c6c5d1e77de15582
Moony <39983684+Moonchasered@users.noreply.github.com>
2025-02-24 22:05:31 +0100

    Refactor TCPDialog to read assigned port from a temporary file, improve connection handling, and enhance message emission.

--- morphoLogicClient/TCPDialog.gd
+++ morphoLogicClient/TCPDialog.gd
@@ -4,7 +4,10 @@ extends Node
 var TCPClient = StreamPeerTCP.new()
 var is_tcp_connected = false
 const SERVER_IP = "127.0.0.1"
-const SERVER_PORT = 6164
+const TEMP_FILE_PATH = "res://temp_port.txt"
+var assigned_port = -1
+var microserver_process = -1
+# const SERVER_PORT = 6164
 signal new_data_arrived(data)
 var root
 
@@ -26,12 +29,29 @@ func run_python_microserver():
 	var microserver_process_id = OS.create_process(python_executable, [script_path])
 	# print(microserver_process_id)
 	if microserver_process_id != -1:
-		print("[GODOT] Python microserver started successfully.")
+		microserver_process = microserver_process_id
+		print("Python microserver started successfully. Process: %s" % microserver_process)
 	else:
-		print("[GODOT] Failed to start Python microserver. :(")
+		print("Failed to start Python microserver. :(")
+
+func read_port_from_temp_file():
+	var file = FileAccess.open(TEMP_FILE_PATH, FileAccess.READ)
+
+	if file:
+		assigned_port = file.get_as_text().strip_edges().to_int()
+		file.close()
+
+		# Delete file after reading
+		DirAccess.remove_absolute(TEMP_FILE_PATH)
+		print("Read port:", assigned_port, "and deleted temp file.")
+	else:
+		print("Port file not found!")
 
 func initialize_connection(client_data: String) -> void:
 	var txt_f = root.get_node("BasicView/MainTextControls/MainTextField")
+	read_port_from_temp_file()
+	if assigned_port == -1:
+		txt_f.draw_new_message(tr("No valid port assigned. Cannot connect."))
 	is_tcp_connected = connect_to_microserver()
 	var message = ""
 	if  is_tcp_connected:
@@ -55,7 +75,7 @@ func _exit_tree():
 	print("[GODOT] Python microserver has been slain.")
 
 func connect_to_microserver() -> bool:
-	var connection_result = TCPClient.connect_to_host(SERVER_IP, SERVER_PORT)
+	var connection_result = TCPClient.connect_to_host(SERVER_IP, assigned_port)
 	if connection_result == OK:
 		print("Connected to %s" % TCPClient.get_connected_host())
 		return true
@@ -65,12 +85,16 @@ func connect_to_microserver() -> bool:
 
 func continously_receive_messages() -> void:
 	while true:
-		var debug = TCPClient.poll()
+		TCPClient.poll()
 		if is_tcp_connected and TCPClient.get_status() == StreamPeerTCP.STATUS_CONNECTED:
 			var available_bytes = TCPClient.get_available_bytes()
 			if available_bytes > 0:
 				var received_data = TCPClient.get_utf8_string(available_bytes)
-				new_data_arrived.emit(received_data)
+				call_deferred("emit_received_data", received_data)
+				# new_data_arrived.emit(received_data)
+
+func emit_received_data(data):
+	new_data_arrived.emit(data)
 
 func send_tcp_message(message: String) -> void:
 	TCPClient.poll()
--- morphoLogicClient/microserver/microserver.py
+++ morphoLogicClient/microserver/microserver.py
@@ -2,17 +2,19 @@
 import json
 import logging
 import socket
+import sys
 
 from logging.handlers import RotatingFileHandler
 from confluent_kafka import Producer, Consumer, KafkaException, KafkaError
 from confluent_kafka.admin import AdminClient, NewTopic
 
-DEBUG = False # Change to false in production
+DEBUG = True  # Change to false in production
 logger = logging.getLogger("mL microserver")
+
 
 class KafkaHandler():
     """Kafka setup for morphoLogic Client"""
-    BOOTSTRAP_SERVER = "localhost:9092" 
+    BOOTSTRAP_SERVER = "localhost:9092"
     PRODUCER_TOPIC = 'serverGlobalTopic'
     producer_config = {
         'bootstrap.servers': BOOTSTRAP_SERVER,
@@ -25,40 +27,51 @@ class KafkaHandler():
         'bootstrap.servers': BOOTSTRAP_SERVER,
 
         # Fixed properties
-        'group.id':          '', # MUST BE updated
+        'group.id':          '',  # MUST BE updated
         'auto.offset.reset': 'earliest'
     }
-    
+
     def __init__(self, client_info):
         client_data = json.loads(client_info)
         client_id = client_data.get("username", "")
+        
         if client_id:
-            # AdminClient
-            self.admin = AdminClient({'bootstrap.servers': self.BOOTSTRAP_SERVER})
-            # Producer
-            self.producer = Producer(self.producer_config)
-            # Consumer
-            self.consumer_config["group.id"] = client_id
-            self.consumer = Consumer(self.consumer_config)
-            # Create new topic for this client
-            self.topic = self._create_new_topic(client_id)
-            # Subscribe to the topic
-            self.consumer.subscribe([self.topic])
-            self.initialized = True
+            try:
+                # AdminClient
+                self.admin = AdminClient(
+                    {'bootstrap.servers': self.BOOTSTRAP_SERVER})
+                
+                # Producer
+                self.producer = Producer(self.producer_config)
+                
+                # Consumer
+                self.consumer_config["group.id"] = client_id
+                self.consumer = Consumer(self.consumer_config)
+                
+                # Create new topic for this client
+                self.topic = self._create_new_topic(client_id)
+                
+                # Subscribe to the topic
+                self.consumer.subscribe([self.topic])
+                
+                self.initialized = True
+            except Exception:
+                logger.exception("Failed to initialize Kafka connection.")
+                self.initialized = False
         else:
             self.initialized = False
-        
+
     def _create_new_topic(self, topic_id):
         nt = NewTopic(topic_id)
         dict_future_topics = self.admin.create_topics([nt])
         try:
-            dict_future_topics[topic_id].result() # Wait for confirmation
+            dict_future_topics[topic_id].result()  # Wait for confirmation
             logger.info('Topic %s created successfully.', topic_id)
         except Exception:
             logger.exception('Failed to create topic %s.', topic_id)
-        
+
         return topic_id
-    
+
     def consume_message(self):
         """
         Consume one message (non-blocking)
@@ -67,7 +80,7 @@ class KafkaHandler():
         msg = self.consumer.poll(0.1)
         if msg is None:
             return ""
-        
+
         if msg.error():
             if msg.error().code() == KafkaError._PARTITION_EOF:
                 # Reached end of partition
@@ -79,7 +92,7 @@ class KafkaHandler():
             msg_val = msg.value().decode("utf-8")
             logger.debug("Consumed message from %s: %s", msg.topic(),  msg_val)
             return msg_val
-        
+
     def produce_message(self, message):
         """
         Send a message to Kafka using the topic initialized at constructor
@@ -87,9 +100,11 @@ class KafkaHandler():
         try:
             # Asynchronous produce; if needed, handle delivery reports with callbacks
             # key is a name of topic, as Server then will send optional naswer to this topic.
-            self.producer.produce (self.PRODUCER_TOPIC, key=self.topic, value=message)
+            self.producer.produce(self.PRODUCER_TOPIC,
+                                  key=self.topic, value=message)
             # producer.poll(0)
-            logger.debug("Produced message to %s: %s", self.PRODUCER_TOPIC, message)
+            logger.debug("Produced message to %s: %s",
+                         self.PRODUCER_TOPIC, message)
         except KafkaException:
             logger.exception("KafkaException while producing message")
 
@@ -99,18 +114,20 @@ class KafkaHandler():
         self.producer.flush(3)
         dict_future_topics = self.admin.delete_topics([self.topic])
         try:
-            dict_future_topics[self.topic].result() # Wait for confirmation
+            dict_future_topics[self.topic].result()  # Wait for confirmation
             logger.info('Topic %s deleted successfully.', self.topic)
         except Exception:
             logger.exception('Failed to delete topic %s.', self.topic)
-        
+
+
 class TCPMicroserver():
     """
     TCP connection for morphoLogic Client.
     It will hang on __init__ waiting for a connection from Godot.
     """
     HOST = "127.0.0.1"
-    PORT = 6164
+    FILE_PORT_HANDLING = "temp_port.txt"
+    port = 0
     conn = None
     client_data = None
 
@@ -119,25 +136,60 @@ class TCPMicroserver():
         Start a TCP server that listens for connections from Godot
         """
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((self.HOST, self.PORT))
-        s.listen()
-        logger.info("Kafka service listening on %s:%s", self.HOST, self.PORT)
-        self.conn, addr = s.accept()
-        logger.info("Connection from %s", addr)
-        self.client_data = self.receive_message()
-        self.conn.setblocking(False)
-        self.conn.settimeout(0.1)
+        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # s.settimeout(60)  # Exit if no connection within 1 min
+        
+        # Enable TCP Keep-Alive
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)  # Enable Keep-Alive
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 30)  # Wait 30 sec before checking
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 10)  # Send a keep-alive every 10 sec
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)  # Send 3 keep-alive probes before dropping
+
+        try:
+            s.bind((self.HOST, self.port))
+
+            # Handling the port to Godot
+            self.port = s.getsockname()[1]
+            with open(self.FILE_PORT_HANDLING, "w") as f:
+                f.write(str(self.port))
+                f.flush()
+
+            s.listen()
+            logger.info("Kafka service listening on %s:%s",
+                        self.HOST, self.port)
+
+            self.conn, addr = s.accept()  # Blocks until connection (but has a timeout)
+            logger.info("Connection from %s", addr)
             
-    def send_message(self, message):
+            self.conn.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            self.conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 30)
+            self.conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 10)
+            self.conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)
+
+            self.client_data = self.receive_message()
+            self.conn.setblocking(False)
+            self.conn.settimeout(0.1)
+        # except socket.timeout:
+        #     logger.warning(
+        #         "No connection received within 60 seconds. Exiting.")
+        #     sys.exit(1)  # Exit if no connection
+        except Exception:
+            logger.exception("Unexpected error setting up TCP connection.")
+            sys.exit(1)
+
+    def send_message(self, message) -> bool:
         """Senf message to Godot via TCP"""
         try:
             coded = message.encode("utf-8")
             self.conn.sendall(coded)
+            logger.info(
+                'Message has been sent via TCP. The message: "%s"', message)
+            return True
         except OSError:
-            return False
+            return True
         except Exception:
             logger.exception("Error sending message via TCP.")
+            return False
 
     def receive_message(self):
         """
@@ -153,12 +205,14 @@ class TCPMicroserver():
         except OSError:
             return False
 
-def cleanup(tcp, kafka = None):
+
+def cleanup(tcp, kafka=None):
     """Cleaning up before exit"""
     if kafka:
         kafka.cleanup()
     tcp.conn.close()
     logger.info("CLEAN UP AND EXIT")
+
 
 def setup_logger():
     """Preparing logger for microserver"""
@@ -174,11 +228,13 @@ def setup_logger():
     logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
     logger.addHandler(log_handler)
 
+
 def main():
     """Main loop"""
     setup_logger()
     # Prepare connection
     k = None
+
     try:
         tcp = TCPMicroserver()
         logger.info("TCP connection created")
@@ -186,12 +242,15 @@ def main():
         if client_data is None:
             cleanup(tcp)
             return
+        
         k = KafkaHandler(client_data)
         if not k.initialized:
+            logger.error("Kafka was not properly initialized.")
             cleanup(tcp, k)
             return
-        logger.info("KAFKA connection created. Moving to main loop.")
         
+        logger.info("KAFKA connection created. Moving to main loop.")
+
         # Main loop
         while True:
             # Kafka -> Godot
@@ -199,7 +258,8 @@ def main():
             if k_message:
                 sent = tcp.send_message(k_message)
                 if not sent:
-                    logger.warning("Message %s has not been sent.", k_message) # Ogarnąć co jeśli nie udało się wysłać.
+                    # Ogarnąć co jeśli nie udało się wysłać.
+                    logger.warning("Message %s has not been sent.", k_message)
             # Kafka <- Godot
             tcp_message = tcp.receive_message()
             if tcp_message:
@@ -213,6 +273,7 @@ def main():
         logger.exception("An error occured, closing down.")
         cleanup(tcp, k)
     cleanup(tcp, k)
-        
+
+
 if __name__ == "__main__":
     main()
